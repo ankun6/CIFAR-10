1d3804e8e8fa1dbe4fcf4d6f72804db3e1092224
AnKun <153471503@qq.com>
2018-11-09 09:27:14 +0800

    ...

--- train.py
+++ train.py
@@ -11,18 +11,19 @@ epochs = 60
 num_classes = 10
 
 # 标签分类字典
-label_dict={0:"airplane",1:"automobile",2:"bird",3:"cat",4:"deer",5:"dog",6:"frog",7:"horse",8:"ship",9:"truck"}
+label_dict = {0: "airplane", 1: "automobile", 2: "bird", 3: "cat", 4: "deer", 5: "dog", 6: "frog", 7: "horse",
+              8: "ship", 9: "truck"}
 
 # 加载数据集
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 # 转换数据类型
 x_train = x_train.astype('float32') / 255
-x_test  = x_test.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
 
 # 转换标签为One-hot
 y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test  = keras.utils.to_categorical(y_test, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 # 搭建网络
 mod = Sequential()
